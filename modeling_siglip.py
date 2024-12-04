39966d5fdba97ff74994975784f2cfd76822c00e
rawathemant246 <hemantrawat246@gmail.com>
2024-12-04 17:46:33 +0530

    starting the Project

---
+++ modeling_siglip.py
@@ -0,0 +1,37 @@
+from typing import Optional, Tuple
+import torch
+import torch.nn as nn
+
+
+
+class SiglipVisionConfig:
+    
+    def __init__(
+        
+        self,
+        hidden_size =768,
+        intermediate_size = 3072,
+        num_hidden_layer = 12,
+        num_attention_heads=12,
+        num_channel=3,
+        image_size=224,
+        patch_size=16,
+        layer_norm_eps=1e-6,
+        attention_dropout=0.0,
+        num_image_tokens : int = None,
+        **kwargs
+    ):
+        
+        super().__init__()
+        
+        self.hidden_size = hidden_size
+        self.intermediate_size = intermediate_size
+        self.num_hidden_layers = num_hidden_layer
+        self.num_attention_heads = num_attention_heads
+        self.num_channels = num_channel
+        self.patch_size = patch_size
+        self.image_size = image_size
+        self.attention_dropout = attention_dropout
+        self.layer_norm_eps = layer_norm_eps
+        self.num_image_tokens = num_image_tokens
+        
