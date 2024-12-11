2ad545408f972204c62e09b38eee52669dffb611
rawathemant246 <hemantrawat246@gmail.com>
2024-12-11 15:55:41 +0530

    added siglipenocderlayer

--- modeling_siglip.py
+++ modeling_siglip.py
@@ -113,7 +113,55 @@ class SiglipMLP(nn.Module):
 
 class SiglipEncoderLayer(nn.Module):
     
-    pass 
+    def __init__(self, config: SiglipVisionConfig):
+        
+        super().__init__()
+        
+        self.embed_dim = config.hidden_size
+        self.self_attn = SiglipAttention(config)
+        self.layer_norm1 = nn.LayerNorm(self.embed_dim, eps=config.layer_norm_eps)
+        self.mlp = SiglipMLP(config)
+        self.layer_norm2 = nn.LayerNorm(self.embed_dim, eps=config.layer_norm_eps)
+    
+    #Ignore copy
+    
+    def forward(self,
+                hidden_states : torch.Tensor,):
+        
+        #residual : [Batch_Size, Num_Patches, Embed_dim]
+        residual = hidden_states
+        
+        # [Batch_size, Num_Patches, Embed_Dim] -> [Batch_Size, Num_Patches, Embed_dim]
+        
+        hidden_states = self.layer_norm1(hidden_states)
+        
+        # [Batch_Size, Num_Patches, Embed_Dim] -> [Batch_Size, Num_Patches, Embed_Dim]
+        
+        hidden_states, _ =self.self_attn(hidden_states=hidden_states)
+        
+        # [Batch_Size, Num_Patches, Embed_Size]
+        
+        hidden_states = residual + hidden_states
+        
+        # residual : [Batch_Size, Num_Patches, Embed_Dim]
+        
+        residual = hidden_states
+        
+        # [Batch_size, Num_Patches, Embed_dim] -> [Batch_Size, Num_Patches, Embed_Dim]
+
+        hidden_states = self.layer_norm2(hidden_states)
+
+        # [Batch_Size, Num_Patches, Embed_Dim] -> [Batch_size, Num_Patches, Embed_Dim]
+
+        hidden_states = self.mlp(hidden_states)
+
+
+        # [Batch_size, Num_Patches, Embed_Dim] -> [Batch_size, Num_Patches, Embed_Dim]
+
+        hidden_states = residual + hidden_states
+
+        return hidden_states
+    
 
 
 
